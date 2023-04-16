527afccaac2dfdb53f1f216a740bfc39c7645799
HappyFeet07 <jack0978239920@gmail.com>
2023-04-16 20:08:44 +0800

    clean

--- test/foundry/MyContract.t.sol
+++ test/foundry/MyContract.t.sol
@@ -13,21 +13,14 @@ contract MyContractTest is Test {
   function setUp() public {
     // TODO:
     // 1. Set user1, user2 
-    user1 = address(0x1);
-    user2 = address(0x2);
     // 2. Create a new instance of MyContract
-    instance = new MyContract(user1, user2);
     // 3. (optional) Set user1 as bob, user2 as alice
-    vm.label(user1, "bob");
-    vm.label(user2, "alice");
   }
 
   function testConstructor() public {
     // TODO: 
     // 1. Assert instance.user1() is user1
     // 2. Assert instance.user2() is user2
-    assertEq(instance.user1(), user1);
-    assertEq(instance.user2(), user2);
   }
 
   function testReceiveEther() public {
@@ -37,10 +30,5 @@ contract MyContractTest is Test {
     // 3. send 1 ether to instance
     // 4. Assert instance has 1 ether in balance
     // 5. stop pretending
-    vm.startPrank(user1);
-    vm.deal(user1, 1 ether);
-    address(instance).call{value: 1 ether}("");
-    assertEq(address(instance).balance, 1 ether);
-    vm.stopPrank();
   }
 }
